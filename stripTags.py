00fa2c003993fe7be1d62f793af934725f064d9a
Ramesh VelurEunni <ramesh.velureunni@gmail.com>
2016-07-13 02:26:52 -0700

    works for one email

--- stripTags.py
+++ stripTags.py
@@ -1,4 +1,6 @@
 # Usage: stripTags <file_name.html>
+# Usage: stripTags <file_name> 
+# Second usage treated as email file with possible embedded html 
 # Output: One text word per line 
 
 import re
@@ -21,25 +23,63 @@ class MyHTMLParser(HTMLParser):
 #	x=1
 
     def handle_data(self, data):
+ 	 oldline = ""
+         val=0
+        
 	 filtered_string = filter(lambda x: x in string.printable, data)
+
+
          words = re.split(r, filtered_string)
          for word in words:
-           if len(word) > 0:
-           	print word.lower()
+           	if len(word) > 0:
+           		print word.lower()
 
 
 def stripTags(text):
  cleaner = Cleaner()
  cleaner.javascript = True # This is True because we want to activate the javascript filter
  cleaner.style = True      # This is True because we want to activate the styles & stylesheet filter
- text = lxml.html.tostring(cleaner.clean_html(lxml.html.parse(text)))
+ text = lxml.html.tostring(cleaner.clean_html(lxml.html.fromstring(text)))
  text = filter(lambda x: x in string.printable, text)
+ entities = [
+          ('nbsp;', ''),
+          ('&#13;', ''),
+          ('202=;', ''),
+         ]
+
+ for before, after in entities:
+    text = text.replace(before, after)
+ oldline=""
+ for line in text.splitlines():
+	if line.endswith('='):
+		nline = line[:-1]
+		oldline += nline
+		continue
+	oldline += line	
+
  parser = MyHTMLParser() 
- parser.feed(text)
+ parser.feed(oldline)
 
  return text
 
+def stripEmail(text):
+  import email
+  msg = email.message_from_string(text)  
+  if msg.is_multipart():
+	for payload in msg.get_payload():
+            stripTags(payload.get_payload())
+  else:
+   	stripTags(msg.get_payload())
+#  text = filter(lambda x: x in string.printable, text)
+# parser = MyHTMLParser() 
+#  parser.feed(text)
+
 
 #main
-with open (sys.argv[1], "r") as myfile:
-        stripTags(myfile)
+fname, file_extension = os.path.splitext(sys.argv[1])
+if '.html' in file_extension or '.htm' in file_extension:
+ with open (sys.argv[1], "r") as myfile:
+        stripTags(myfile.read())
+else: #assume email format
+ with open (sys.argv[1], "r") as myfile:
+	stripEmail(myfile.read())
